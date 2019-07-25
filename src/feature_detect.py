40c175ff903bd00a36910f7db297d3b17e11a93d
freeberg <oskar.friberg95@gmail.com>
2019-07-25 17:48:19 +0900

    Fixed SRAD and how to get out masks from manuell segmentation

--- src/feature_detect.py
+++ src/feature_detect.py
@@ -6,12 +6,12 @@ import cv2
 import csv
 from srad import srad
 
-magnus = (69, 70, "Magnus", (100,500), (200, 600))
-tobias = (109, 169, "Tobias", (136,488), (186,660))
+magnus = (43, 70, "Magnus", (100,500), (200, 600))
+tobias = (109, 168, "Tobias", (136,488), (186,660))
 roger = (223, 283, "Roger", (100,500), (200, 600))
 FP1 = (18, 99, "FP1",(100,500), (200, 600))
-FP2 = (105, 187, "FP2",(100,500), (200, 600))
-patients = [magnus, tobias, roger, FP1, FP2]
+FP2 = (105, 155, "FP2",(100,500), (200, 600))
+patients = [FP2]
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
@@ -38,7 +38,7 @@ def expand_to_c3(img):
     img_c3 = np.ones((rows,cols,3))
     for i in range(rows):
         for j in range(cols):
-            img_c3[i][j][:] = img[i][j] * img_c3[i][j][:]
+            img_c3[i][j][:] = 255 * np.log(img[i][j]) * img_c3[i][j][:]
 
     return img_c3 
 
@@ -50,18 +50,19 @@ for patient in patients:
     out_dir = "dataset/" + patient[2] + "_SRAD/"
     print("Producing SRAD for " + patient[2])
     for i in range(patient[1]-patient[0]):
-
+        if patient == FP2 and i == 154:
+            continue
         image, rows, cols = load_dicom_image(patient, i + patient[0])
         if('h' in sav_or_show):
             # clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
-            # clahe2 = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(4,4))
-            # cl1 = cv2.equalizeHist(image)
-            # cl2 = clahe2.apply(image)
+            clahe2 = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(4,4))
+            cl1 = cv2.equalizeHist(image)
+            cl2 = clahe2.apply(image)
             scal_img = np.exp(image/255)
-            # scal_img2 = np.exp(cl2/255)
-            srad_img = srad(scal_img, rows, cols, 20, 0.1)
-            # srad_img2 = srad(scal_img2, rows, cols, 120, 0.05)
+            scal_img2 = np.exp(cl2/255)
+            srad_img = srad(scal_img, rows, cols, 60, 0.1)
+            srad_img2 = srad(scal_img2, rows, cols, 60, 0.1)
             srad_img_c3 = expand_to_c3(srad_img)
-            # srad_img2_c3 = expand_to_c3(srad_img2)
+            srad_img2_c3 = expand_to_c3(srad_img2)
             cv2.imwrite(out_dir + patient[2] + str(i + patient[0]) + 'ORG.png', srad_img_c3)
-            # cv2.imwrite(out_dir + patient[2] + str(i + patient[0]) + 'CLAHE.png',srad_img2_c3)
+            cv2.imwrite(out_dir + patient[2] + str(i + patient[0]) + 'CLAHE.png',srad_img2_c3)
